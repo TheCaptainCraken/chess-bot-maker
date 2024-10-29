b18de431199498c735cf788094cf7d9cddcc90f3
Pietro Agnoli <87072344+TheCaptainCraken@users.noreply.github.com>
2024-10-29 15:50:56 +0100

    added chessboard js

---
+++ static/index.js
@@ -0,0 +1,12 @@
+const ruyLopez = 'r1bqkbnr/pppp1ppp/2n5/1B2p3/4P3/5N2/PPPP1PPP/RNBQK2R'
+
+const config = {
+    pieceTheme: 'pieces/{piece}.png',
+    position: ruyLopez,
+    draggable: true,
+    moveSpeed: 'slow',
+    snapbackSpeed: 500,
+    snapSpeed: 100,
+}
+
+const board = Chessboard('myBoard', config)
