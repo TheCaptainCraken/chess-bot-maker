7ea49df21b2068ddcaefac9171f55d68f4db4837
Pietro Agnoli <87072344+TheCaptainCraken@users.noreply.github.com>
2024-10-29 19:14:45 +0100

    added frontend using actix

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,16 @@
-use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
-
-#[get("/")]
-async fn hello() -> impl Responder {
-    HttpResponse::Ok().body("Hello world!")
-}
-
-#[post("/echo")]
-async fn echo(req_body: String) -> impl Responder {
-    HttpResponse::Ok().body(req_body)
-}
-
-async fn manual_hello() -> impl Responder {
-    HttpResponse::Ok().body("Hey there!")
-}
+use actix_files as fs;
+use actix_web::{middleware, App, HttpServer};
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    env_logger::init_from_env(env_logger::Env::new().default_filter_or("info"));
+
+    log::info!("starting HTTP server at http://localhost:8080");
+
     HttpServer::new(|| {
         App::new()
-            .service(hello)
-            .service(echo)
-            .route("/hey", web::get().to(manual_hello))
+            .wrap(middleware::Logger::default())
+            .service(fs::Files::new("/", "./static/").index_file("index.html"))
     })
     .bind(("127.0.0.1", 8080))?
     .run()
